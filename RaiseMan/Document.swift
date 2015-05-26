8ca595e681d8ecb44b36b439f30d3e0d42200dda
Mike Zornek <mike@clickablebliss.com>
2015-05-26 11:44:26 -0400

    Adding undo support (ch11)

--- RaiseMan/Document.swift
+++ RaiseMan/Document.swift
@@ -1,8 +1,24 @@
 import Cocoa
 
-class Document: NSDocument {
+private var KVOContext: Int = 0
+
+class Document: NSDocument, NSWindowDelegate {
     
-    var employees: [Employee] = []
+    @IBOutlet weak var tableView: NSTableView!
+    @IBOutlet weak var arrayController: NSArrayController!
+    
+    var employees: [Employee] = [] {
+        willSet {
+            for employee in employees {
+                stopObservingEmployee(employee)
+            }
+        }
+        didSet {
+            for employee in employees {
+                startObservingEmployee(employee)
+            }
+        }
+    }
     
     override init() {
         super.init()
@@ -27,5 +43,99 @@ class Document: NSDocument {
     override func readFromData(data: NSData, ofType typeName: String, error outError: NSErrorPointer) -> Bool {
         return false
     }
+    
+    //MARK: - Actions
+    
+    @IBAction func addEmployee(sender: NSButton) {
+        let windowController = windowControllers[0] as! NSWindowController
+        let window = windowController.window!
+        
+        let endEditing = window.makeFirstResponder(window)
+        if !endEditing {
+            println("unable to end editing")
+            return
+        }
+        
+        let undo = undoManager!
+        if undo.groupingLevel > 0 {
+            undo.endUndoGrouping()
+            undo.beginUndoGrouping()
+        }
+        
+        let employee = arrayController.newObject() as! Employee
+        arrayController.addObject(employee)
+        arrayController.rearrangeObjects()
+        
+        let sortedEmployees = arrayController.arrangedObjects as! [Employee]
+        let row = find(sortedEmployees, employee)!
+        println("start editing of \(employee) at \(row)")
+        tableView.editColumn(0, row: row, withEvent: nil, select: true)
+    }
+    
+    
+    //MARK: - KVC
+    
+    func insertObject(employee: Employee, inEmployeesAtIndex index: Int) {
+        println("adding \(employee) to the employees array")
+        
+        let undo = undoManager!
+        undo.prepareWithInvocationTarget(self).removeObjectFromEmployeesAtIndex(employees.count)
+        if !undo.undoing {
+            undo.setActionName("Add Person")
+        }
+        employees.append(employee)
+    }
+    
+    func removeObjectFromEmployeesAtIndex(index: Int) {
+        let employee = employees[index]
+        println("removing \(employee) from the employees array")
+        
+        let undo = undoManager!
+        undo.prepareWithInvocationTarget(self).insertObject(employee, inEmployeesAtIndex: index)
+        if !undo.undoing {
+            undo.setActionName("Remove Person")
+        }
+        
+        employees.removeAtIndex(index)
+    }
+    
+    //MARK: - KVO
+    
+    func startObservingEmployee(employee: Employee) {
+        employee.addObserver(self, forKeyPath: "name", options: .Old, context: &KVOContext)
+        employee.addObserver(self, forKeyPath: "raise", options: .Old, context: &KVOContext)
+    }
+    
+    func stopObservingEmployee(employee: Employee) {
+        employee.removeObserver(self, forKeyPath: "name", context: &KVOContext)
+        employee.removeObserver(self, forKeyPath: "raise", context: &KVOContext)
+    }
+    
+    override func observeValueForKeyPath(keyPath: String, ofObject object: AnyObject, change: [NSObject : AnyObject], context: UnsafeMutablePointer<Void>) {
+        if context != &KVOContext {
+            // If the context does not match, this message
+            // must be intended for our superclass.
+            super.observeValueForKeyPath(keyPath, ofObject: object, change: change, context: context)
+            return
+        }
+        
+        var oldValue: AnyObject? = change[NSKeyValueChangeOldKey]
+        if oldValue is NSNull {
+            oldValue = nil
+        }
+        
+        let undo: NSUndoManager = undoManager!
+        println("oldValue=\(oldValue)")
+        undo.prepareWithInvocationTarget(object).setValue(oldValue, forKeyPath: keyPath)
+    }
+    
+    //MARK: - NSWindowDelegate
+    
+    func windowWillClose(notification: NSNotification) {
+        employees = []
+    }
+    
+    
+    
 
 }
